aea7f52e6aef5a3002a01a8756492e2b97b5475a
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-02-02 21:55:26 +0100

    Adding missing file

---
+++ src/lib/components/HomeGrid.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import Control from '$lib/images/control.svg?component';
+	import Encrypted from '$lib/images/encrypted.svg?component';
+	import Undercover from '$lib/images/undercover.svg?component';
+	import SocialMedia from '$lib/images/social-media.svg?component';
+	import Bot from '$lib/images/bot.svg?component';
+	import BitcoinPhone from '$lib/images/bitcoin-phone.svg?component';
+</script>
+
+<div class="flex flex-col items-center gap-4 text-center">
+	<Control width="100" height="100" />
+	<h3 class="font-bold">You Are In Control</h3>
+	<p>
+		Built on open internet protocols, there is no platform that can ban or censor you. You are in
+		control of your data & speech.
+	</p>
+</div>
+
+<div class="flex flex-col items-center gap-4 text-center">
+	<Encrypted width="100" height="100" />
+	<h3 class="font-bold">Encrypted</h3>
+	<p>End-to-End encrypted private messaging. Keep big tech out of your DMs.</p>
+</div>
+
+<div class="flex flex-col items-center gap-4 text-center">
+	<Undercover width="100" height="100" />
+	<h3 class="font-bold">No Registration Required</h3>
+	<p>
+		Creating an account doesn't require a phone number, email or name. Get started right away with
+		zero friction.
+	</p>
+</div>
+
+<div class="flex flex-col items-center gap-4 text-center">
+	<SocialMedia width="100" height="100" />
+	<h3 class="font-bold">No Centralized Servers</h3>
+	<p>
+		Messages are distributed via decentralized relays. No need to run any infrastructure and there
+		are no single points of failure. Simple!
+	</p>
+</div>
+
+<div class="flex flex-col items-center gap-4 text-center">
+	<Bot width="100" height="100" />
+	<h3 class="font-bold">Programmable</h3>
+	<p>
+		Easily integrate bots that automate your life or business. Get notified when your servers go
+		down, repost to your team and collaborate in realtime.
+	</p>
+</div>
+
+<div class="flex flex-col items-center gap-4 text-center">
+	<BitcoinPhone width="100" height="100" />
+	<h3 class="font-bold">Earn Money</h3>
+	<p>
+		Show your appreciation and stack sats with Bitcoin & ⚡️ Lightning, the native currency of the
+		internet.
+	</p>
+</div>
