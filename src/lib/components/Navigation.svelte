e1f9d7668e720cdf1d465047470208149970501a
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-02-02 21:58:58 +0100

    Update URL to Nostr.how

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <nav class="flex flex-row gap-8 items-center text-lg">
-	<a href="https://www.nostr.how">Learn about Nostr</a>
+	<a href="http://www.nostr.how">Learn about Nostr</a>
 	<a href="/logs">Logs</a>
 	<a href="https://github.com/damus-io/damus">
 		<Github width="24" height="24" fill="white" />
